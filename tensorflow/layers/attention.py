ce45ad6b8e712b5539cbbc0637491918ad4740f9
rluver <skyd0608@gmail.com>
2022-06-23 00:46:10 +0900

    Implements attention

---
+++ tensorflow/layers/attention.py
@@ -0,0 +1,34 @@
+from ...config.config import model_config
+
+from tensorflow.keras.layers import Input, Layer, Attention, Dropout, Dense
+from tensorflow.keras.models import Model
+
+
+class AttentionBlock(Layer):
+    def __init__(self, ff_dense, config=model_config):
+        super(AttentionBlock, self).__init__()
+        self.config = model_config
+        self.attention = Attention(use_scale=True)
+        self.dense = Dense(units=ff_dense)
+        self.dropout = Dropout(rate=model_config.dropout_rate)
+
+    def call(self, x):
+        attention_output = self.attention([x, x, x])
+        x += attention_output
+        x = self.dropout(x)
+
+        dense_output = self.dense(x)
+        x += dense_output
+        x = self.dropout(x)
+
+        return x
+
+
+def build_attention(x, config=model_config):
+    input = Input(shape=x.shape[1:])
+
+    output = input
+    for _ in range(config.attention_stacks):
+        output = AttentionBlock(input.shape[-1], config)(output)
+    
+    return Model(input, output)
