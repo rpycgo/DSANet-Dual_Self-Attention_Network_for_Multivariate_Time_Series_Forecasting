7157b2f955ea2726fba1bd3176065b6a7b7a1765
rluver <skyd0608@gmail.com>
2022-06-23 20:21:35 +0900

    fix: Implements merged vectors by tile

--- tensorflow/layers/global_temporal_convolution.py
+++ tensorflow/layers/global_temporal_convolution.py
@@ -1,7 +1,7 @@
 from ...config.config import model_config
 
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Layer, Conv1D, Dropout
+from tensorflow.keras.layers import Input, Layer, Conv2D, Dropout
 from tensorflow.keras.models import Model
 
 
@@ -9,13 +9,15 @@ class GlobalTemporalConvolutionBlock(Layer):
     def __init__(self, config=model_config):
         super(GlobalTemporalConvolutionBlock, self).__init__()
         self.config = model_config
-        self.conv = Conv1D(filters=config.n_g, kernel_size=config.time_seq, activation='relu')
+        self.conv = Conv2D(filters=config.n_g, kernel_size=config.time_seq, activation='relu')
         self.dropout = Dropout(rate=model_config.dropout_rate)
 
     def call(self, x):
         x = self.conv(x)
-        x = tf.transpose(x, perm=(0, 2, 1))
-       
+        x = self.dropout(x)
+        # to do: implements exact layers of merged by vectors
+        x = tf.tile(x, [1, self.config.D, 1])
+
         return x
 
 
