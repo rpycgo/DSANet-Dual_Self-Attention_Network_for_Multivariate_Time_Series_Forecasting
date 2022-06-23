d68d754f3771a646209f2349e5bdfb056c79f5c7
rluver <skyd0608@gmail.com>
2022-06-23 21:03:46 +0900

    replace attention with MultiHeadAttention and add layer normalization

--- tensorflow/layers/attention.py
+++ tensorflow/layers/attention.py
@@ -1,6 +1,6 @@
 from ...config.config import model_config
 
-from tensorflow.keras.layers import Input, Layer, Attention, Dropout, Dense
+from tensorflow.keras.layers import Input, Layer, MultiHeadAttention, Dropout, Dense, LayerNormalization
 from tensorflow.keras.models import Model
 
 
@@ -8,20 +8,27 @@ class AttentionBlock(Layer):
     def __init__(self, config=model_config):
         super(AttentionBlock, self).__init__()
         self.config = model_config
-        self.attention = Attention(use_scale=True)
-        self.dense = Dense(units=ff_dense, activation='relu')
+        self.attention = MultiHeadAttention(
+            num_heads=config.n_heads, 
+            key_dim=config.n_g//config.n_heads,
+            value_dim=config.n_g//config.n_heads
+            )
+        self.layer_normalization = LayerNormalization()
+
         self.dropout = Dropout(rate=model_config.dropout_rate)
 
     def build(self, input_shape):
         self.dense = Dense(units=input_shape[-1], activation='relu')
 
     def call(self, x):
-        attention_output = self.attention([x, x, x])
+        attention_output = self.attention(x, x, x)
         x += attention_output
+        x = self.layer_normalization(x)
         x = self.dropout(x)
 
         dense_output = self.dense(x)
         x += dense_output
+        x = self.layer_normalization(x)
         x = self.dropout(x)
 
         return x
